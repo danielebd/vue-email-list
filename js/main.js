f8a5bc8607e50ddaebd4173bfcf12c188955d5c4
danielebd <danielebalebalestro@gmail.com>
2023-03-21 17:41:05 +0100

    bonus

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ createApp({
         // console.log(this.message);
 
         //fare cose
+        
         for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) => {
@@ -20,10 +21,10 @@ createApp({
                     // console.log(response.data);
                     // console.log(this);
                     this.listEmail.push(response.data.response);
-
                 });
         }
         console.log(this.listEmail);
+
 
     }
 
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 #### html
-creare un ul e con v-for generare 10 li
+creare un ul e con v-for generare 10 li con contenuto l'argomento dell'array listEmail
 
 #### js 
-all'interno 
+all'interno di before mount ciclare 10 mail random e pusharle nell'array vuoto listEmail
